fd988038513c5d94ce1223b5969e943cc0034156
hauck29 <anthony.hauck@aol.com>
2022-01-25 14:16:39 -0800

    photoComments inprog

--- react-app/src/components/DisplayPhoto/DisplayPhoto.js
+++ react-app/src/components/DisplayPhoto/DisplayPhoto.js
@@ -2,27 +2,43 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPhotos, deletePhoto, editPhoto } from "../../store/photos";
 import { NavLink } from "react-router-dom";
+import "./DisplayPhoto.css";
+import { getPhotoComments } from "../../store/photoComments";
 
 function DisplayPhoto({ photoId }) {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getPhotos());
+    dispatch(getPhotos(), dispatch(getPhotoComments()));
   }, [dispatch]);
   const photos = useSelector((state) => state.photos);
+  const photoComments = useSelector((state) =>
+    Object.values(state.photoComments)
+  );
   const photo = photos[photoId];
   console.log("$$$$$$$$$$", photos, photo);
   return (
-      <>
-        <div className="rtpBack">
+    <>
+      <div className="rtpBack">
         <NavLink className="rtp" to={"/"}>
           Return to Photos
         </NavLink>
+      </div>
+      <div className="displayPhoto">
+        <img
+          src="https://image.shutterstock.com/image-photo/photo-owl-macro-photography-high-260nw-1178957458.jpg"
+          alt=""
+        ></img>
+      </div>
+      <div className="createCommentDiv">
+        <div clasName="commentsDiv">
+            {photoComments?.reverse().map((photoComment) => (
+          <div className="commentDivB" key={photoComment.id}>
+              {photoComment.pcontent}
+          </div>
+            ))}
         </div>
-    <img
-      src="https://image.shutterstock.com/image-photo/photo-owl-macro-photography-high-260nw-1178957458.jpg"
-      alt=""
-    ></img>
-      </>
+      </div>
+    </>
   );
   //   return <img src={photo.url} alt=""></img>;
 }
