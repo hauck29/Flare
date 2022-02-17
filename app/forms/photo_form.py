dfffb8ea2df79f4c7bded4f3548d01fc6a584163
hauck29 <anthony.hauck@aol.com>
2022-02-17 10:26:13 -0800

    aws functioning

--- app/forms/photo_form.py
+++ app/forms/photo_form.py
@@ -1,9 +1,9 @@
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, FileField
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 
 class PhotoForm(FlaskForm):
 
 	user_id=IntegerField("user_id", validators=[DataRequired()])
-	url=StringField("url", validators=[DataRequired()])
+	image=FileField("image", validators=[DataRequired()])
 	caption=StringField("caption")
